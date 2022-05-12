b125ab9c93c42ce945b375b1c4beb2602b48ca70
remasbashanfar <remasbashanfar1@gmail.com>
2022-05-11 17:33:23 -0700

    added page linking, buttons to pages, and page outlines

---
+++ react-connectucla/src/pages/home/feed.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import {Link } from "react-router-dom";
+
+    function Feed() {
+
+        return (
+          <div>
+              This is the feed page
+          </div>
+        );
+
+    }
+
+    export default Feed;
