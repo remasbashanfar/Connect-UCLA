4a4ef3d4386e80dafd1e51b4556492ad0db2ccd6
Twicster <mathias.duedahl@gmail.com>
2022-05-14 23:57:17 -0700

    retrievePosts working, create card for each post

--- react-connectucla/src/pages/Home.js
+++ react-connectucla/src/pages/Home.js
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom'
 import NavBar from '../components/navbar.js'
 import Card from '../components/card.js'
 import PostAPI from '../services/post.js'
@@ -6,7 +5,7 @@ import React, { useState, useEffect } from "react";
 // Homepage doubles as the feed.
 
 export default function Home() {
-    // Variables
+    // Variables + hooks
     const [posts, setPosts] = useState([]);
 
     // Do on render
@@ -14,11 +13,13 @@ export default function Home() {
         retrievePosts();
     }, []);
     
+    // Get all posts from server
     const retrievePosts = () => {
         PostAPI.getAll()
         .then(response => {
-            console.log(response.data);
-            setPosts(response.data.posts);
+            // Transform Json since Json not allowed
+            const Transform = response.data.map(d => [d._id, d.title, d.content, d.imgurl, d.category]);
+            setPosts(Transform);
         })
         .catch(error => console.log(error));
     };
@@ -27,22 +28,20 @@ export default function Home() {
         <div >
             <NavBar></NavBar>
             <h1>Connect UCLA</h1>
-            <h2>Welcome home! {posts}</h2>
+            <h2>Welcome home!</h2>
 
-            
-
-            <Card 
-            image="https://upload.wikimedia.org/wikipedia/commons/8/8c/Cow_%28Fleckvieh_breed%29_Oeschinensee_Slaunger_2009-07-07.jpg" 
-            title="Test card" 
-            description="Chill with the cows on the pasture, byo MOOO"
-            />
-
-            <br/>
-            <button style={{display:'flex', alignItems:'center', justifyContent:'center', right:'80px'}}><Link to='/about'>About</Link> </button> 
-            <br/>
-            <button style={{display:'flex',alignItems:'center'}}><Link to='/menu'>Menu</Link> </button> 
-            <br/>
-            <button style={{display:'flex',alignItems:'center'}}><Link to='/login'>Login</Link> </button> 
+            {
+            posts.map(post => {
+                return (
+                <Card 
+                key={post[0]}
+                image={post[3]} 
+                title={post[1]}
+                description={post[2]}
+                />
+                );
+            })
+            }
         </div>
     )
 }
