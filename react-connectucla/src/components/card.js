fefc13ea65812d82462345fd20e2a8975dc866cc
Twicster <mathias.duedahl@gmail.com>
2022-05-17 09:41:48 -0700

    Entire card is now a button

--- react-connectucla/src/components/card.js
+++ react-connectucla/src/components/card.js
@@ -2,6 +2,7 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
+import CardActionArea from '@mui/material/CardActionArea';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
@@ -10,6 +11,7 @@ import Typography from '@mui/material/Typography';
 export default function ImgMediaCard(props) {
   return (
     <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
       <CardMedia
         component="img"
         height="140"
@@ -23,6 +25,7 @@ export default function ImgMediaCard(props) {
           {props.description}
         </Typography>
       </CardContent>
+      </CardActionArea>
       <CardActions>
         <Button size="small">Interested</Button>
         <Button size="small">See Post</Button>
