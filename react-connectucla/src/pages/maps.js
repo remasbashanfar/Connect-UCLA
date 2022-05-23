ed9011932188e53b759996b56e35fffc08a63bdc
samir01 <samir.os.abdalla@gmail.com>
2022-05-22 20:48:43 -0700

    maps.js now displays google maps and is centered on UCLA

--- react-connectucla/src/pages/maps.js
+++ react-connectucla/src/pages/maps.js
@@ -8,6 +8,16 @@ import {
 import { renderMatches } from 'react-router-dom'
 import "@reach/combobox/styles.css"
 
+const mapContainerStyle = {
+    width: '100vw',
+    height: '100vh',
+}
+
+const center = {
+    lat:34.068920,
+    lng:-118.445183,
+}
+
 const libraries = ["places"]
 export default function Maps() {
     const {isLoaded, loadError} = useLoadScript({
@@ -20,7 +30,14 @@ export default function Maps() {
     if(!isLoaded)
         return "Loading Maps"
     return(
-        <div>Hi</div>
+        <div>
+            <GoogleMap 
+                mapContainerStyle={mapContainerStyle}
+                zoom = {16}
+                center = {center}
+                options = {options}>
+            </GoogleMap>
+        </div>
     )
 
 }
