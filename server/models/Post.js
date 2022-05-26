a9b400b6d29a0aeac02a9c6254cdda059b1e6d46
Twicster <mathias.duedahl@gmail.com>
2022-05-26 00:13:13 -0700

    Updated post schema

--- server/models/Post.js
+++ server/models/Post.js
@@ -5,20 +5,41 @@ const PostSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    category: {
+    location:{
         type: String,
         required: true,
     },
-    content: {
+    startTime:{
         type: String,
         required: true,
     },
     imgurl: {
         type: String,
         required: false,
+    },
+    RSVP_counter: {
+        type: Array,
+        required: true,
+    },
+    tags: {
+        type: Array,
+        required: false,
+    },
+    author: {
+        type: String,
+        required: true,
+    },
+    content:{
+        type: String,
+        required: true,
+    },
+    endTime: {
+        type: String,
+        required: false,
     }},
     {collection: "posts"}
-)
+);
+ 
 
 // changed from "posts" to "Post"
 const PostModel = mongoose.model("Post", PostSchema)
