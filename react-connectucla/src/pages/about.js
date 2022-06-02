c6cfba06bcd9802503af355395a8191c78d1f585
remasbashanfar <remasbashanfar1@gmail.com>
2022-06-01 19:51:41 -0700

    added intro page

--- react-connectucla/src/pages/about.js
+++ react-connectucla/src/pages/about.js
@@ -1,12 +1,12 @@
 import React from "react";
 import NavBar from '../components/navbar.js'
 import FollowFeed from '../components/profile-feed/follow-feed'
+import IntroPage from "./introPage.js";
     function About() {
 
         return (
           <div>
-              <NavBar></NavBar>
-              This is the about page
+              <IntroPage/>
               <FollowFeed username="JungHoseok"/>
           </div>
         );
